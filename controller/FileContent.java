ad2f4fe0be2cf825e4325492fd42d4a35baf094d
pfolus <imjanczalover18@gmail.com>
2017-09-11 10:44:30 +0200

    Add methods in FileContent class

--- controller/FileContent.java
+++ controller/FileContent.java
@@ -1,22 +1,28 @@
 import java.util.Iterator;
+import java.util.Scanner;
+
 
 public class FileContent implements IterableText {
+    private String fileName;
 
     public FileContent(String fileName) {
-
+        this.fileName = fileName;
     }
 
     public Iterator<String> charIterator() {
+        Iterator<String> charIterator = new CharIterator<String>();
 
+        return charIterator;
     }
 
     public Iterator<String> wordIterator() {
+        Iterator<String> wordIterator = new WordIterator<String>();
 
+        return wordIterator;
     }
 
     public String getFileName() {
-
+        return this.fileName;
     }
 
-    
 }
