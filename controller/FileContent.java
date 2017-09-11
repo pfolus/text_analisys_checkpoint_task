89c732de5462504fdbdd90b9f59b8e9537b8a00a
pfolus <imjanczalover18@gmail.com>
2017-09-11 10:10:31 +0200

    Add FileContent class pattern

---
+++ controller/FileContent.java
@@ -0,0 +1,22 @@
+import java.util.Iterator;
+
+public class FileContent implements IterableText {
+
+    public FileContent(String fileName) {
+
+    }
+
+    public Iterator<String> charIterator() {
+
+    }
+
+    public Iterator<String> wordIterator() {
+
+    }
+
+    public String getFileName() {
+
+    }
+
+    
+}
