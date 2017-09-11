88626b33ff90683ef494ae0c3a89189f32fffaed
pfolus <imjanczalover18@gmail.com>
2017-09-11 10:11:02 +0200

    Add StatisticalAnalisys class pattern

---
+++ controller/StatisticalAnalisys.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+
+public class StatisticalAnalisys {
+
+    public Iterator<String> StatisticalAnalisys() {
+
+    }
+
+    public int countOf(String... stringInput) {
+
+    }
+
+    public int dictionarySize(){
+
+    }
+
+    public int size() {
+
+    }
+
+    public Set<String> occurMoreThan(Integer times) {
+        
+    }
+
+}
